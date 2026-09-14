3bc9166667589e902071535a44d1eaa6b9696559
CMCalamity <nguyenductoank17@siu.edu.vn>
2026-09-14 08:23:12 +0700

    fix30

--- LyMapFood/Program.cs
+++ LyMapFood/Program.cs
@@ -1,27 +1,36 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Bật dịch vụ MVC và Session
 builder.Services.AddControllersWithViews();
-builder.Services.AddSession(); // Bật dịch vụ Session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+// Cho phép nạp file index.html làm trang chủ mặc định
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+// UseSession PHẢI nằm TRƯỚC UseAuthorization và MapControllerRoute
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.UseSession(); // Cho phép dùng Session
+
 app.Run();
